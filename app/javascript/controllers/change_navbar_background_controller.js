a4ef938507096c88475870d9b65fa20f7f94e192
Benjamin Liet <benjamin.liet.dev@gmail.com>
2022-08-11 22:30:21 +0200

    implemented full responsiveness

--- app/javascript/controllers/change_navbar_background_controller.js
+++ app/javascript/controllers/change_navbar_background_controller.js
@@ -18,39 +18,45 @@ export default class extends Controller {
   }
 
   modifyNavbar(e) {
-    if(window.innerWidth <= 900) return
-
-    if (window.scrollY > 30) {
-      // Make first navbar disapear to make second navbar apppear (only background for now)
-      this.principalTarget.classList.add('disapear');
-      this.secondaryTarget.classList.remove('disapear');
-
-      try {
-        // Next section is to make the dropdown navbar items disapear when scroll if active
-        this.mainDropdownListTarget.classList.add('disapear');
-        this.navbarItemTarget.style.background = 'transparent';
-        this.navbarItemTarget.style.border = '0px solid black';
-      } catch {
-
-      }
-
-      if (window.scrollY > 40) {
-        // Here is the second navbar text appearing with smooth transition
-        this.shortLogoTarget.classList.add('active');
-        this.shortItemsTarget.classList.add('active');
+    if(window.innerWidth <= 900) {
+      if (window.scrollY > 30) {
+        this.mobileTarget.classList.add('box-shadow');
+      } else {
+        this.mobileTarget.classList.remove('box-shadow');
       }
     } else {
-      // Make the first navbar re-appearing when scroll up
-      this.principalTarget.classList.remove('disapear');
-      this.secondaryTarget.classList.add('disapear');
-      this.shortLogoTarget.classList.remove('active');
-      this.shortItemsTarget.classList.remove('active');
+      if (window.scrollY > 30) {
+        // Make first navbar disapear to make second navbar apppear (only background for now)
+        this.principalTarget.classList.add('disapear');
+        this.secondaryTarget.classList.remove('disapear');
 
-      try {
-        // This section is to make the user dropdown disapear when scroll up if active
-        this.secondDropdownListTarget.classList.add('disapear');
-      } catch {
+        try {
+          // Next section is to make the dropdown navbar items disapear when scroll if active
+          this.mainDropdownListTarget.classList.add('disapear');
+          this.navbarItemTarget.style.background = 'transparent';
+          this.navbarItemTarget.style.border = '0px solid black';
+        } catch {
 
+        }
+
+        if (window.scrollY > 40) {
+          // Here is the second navbar text appearing with smooth transition
+          this.shortLogoTarget.classList.add('active');
+          this.shortItemsTarget.classList.add('active');
+        }
+      } else {
+        // Make the first navbar re-appearing when scroll up
+        this.principalTarget.classList.remove('disapear');
+        this.secondaryTarget.classList.add('disapear');
+        this.shortLogoTarget.classList.remove('active');
+        this.shortItemsTarget.classList.remove('active');
+
+        try {
+          // This section is to make the user dropdown disapear when scroll up if active
+          this.secondDropdownListTarget.classList.add('disapear');
+        } catch {
+
+        }
       }
     }
   }
